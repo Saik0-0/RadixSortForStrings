af1728df0f45e0fda355b95ef76456ac6b021a4c
Litvinenko Alexandra <alexandralit09022005@gmail.com>
2024-09-12 22:51:18 +0300

    step 4

--- src/main/java/radixSort.java
+++ src/main/java/radixSort.java
@@ -1,7 +1,30 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class radixSort {
-    public static List<String> getSortedList(String fileName) {
+    public static String getSortedList(String fileName) throws IOException {
+        List<String> file;
+        try {
+            file = Files.readAllLines(Path.of(fileName));
+        } catch (IOException e) {
+            System.out.println("С файлом какая-то проблема((");
+            return null;
+        }
+        return String.valueOf(file);
+    }
+
+    public static void digitalSort(List<String> words) {
+        char[] chars = new char[26];
+        for (int i = 0; i < chars.length; i++) {
+            chars[i] = (char) ('a' + i);
+        }
+
 
     }
 }
