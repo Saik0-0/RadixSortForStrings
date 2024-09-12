4dd8c439616c1a2640b59486a43f4244319c1182
Litvinenko Alexandra <alexandralit09022005@gmail.com>
2024-09-13 00:19:20 +0300

    step 5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,5 +9,8 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+        List<String> words = Arrays.asList("apple", "banana", "kiwi", "peach", "grape", "berry");
+        radixSort.digitalSort(words);
     }
 }
--- src/main/java/radixSort.java
+++ src/main/java/radixSort.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -25,6 +26,43 @@ public class radixSort {
             chars[i] = (char) ('a' + i);
         }
 
+        //  ищем самое длинное слово
+        int len = 0;
+        for (String word: words) {
+            if (word.length() > len) {
+                len = word.length();
+            }
+        }
+        
+        //  более короткие слова дополним нулями слева
+        for (int i = 0; i < words.toArray().length; i++) {
+            if (words.get(i).length() < len) {
+                words.set(i, words.get(i) + "0".repeat(len - words.get(i).length()));
+            }
+        }
 
+        for (int i = len - 1; i >= 0; i--) {
+
+            List<List<String>> forSort = new ArrayList<>();
+            for (int j = 0; j < 26; j++) {
+                forSort.add(new ArrayList<>());
+            }
+
+            for (String word: words) {
+                if (word.charAt(i) != '0') {
+                    forSort.get(word.charAt(i) - 'a').add(word);
+                }
+                else {
+                    forSort.get('z' - 'a').add(word);
+                }
+            }
+
+            words.clear();
+            for (List<String> sorted: forSort) {
+                words.addAll(sorted);
+            }
+        }
+
+        System.out.println(words);
     }
 }
